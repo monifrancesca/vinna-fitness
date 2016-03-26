bf43bfd66c2e43dd6e2b5c4bb5668f29b49cbc08
atousley <annette.tousley@gmail.com>
2016-03-26 10:50:08 -0500

    posting to the database, needed to input fake data and fake id to mimic

--- public/scripts/factories/data-factory.js
+++ public/scripts/factories/data-factory.js
@@ -1,8 +1,10 @@
 myApp.factory('DataFactory', ['$http', function($http) {
 
+    var fakeId = 'a';
+
     var postMedical = function(data) {
         console.log('factory data', data);
-        $http.post('/medical/', data).then(function(response){
+        $http.post('/medical/' + fakeId, data).then(function(response){
         });
     };
 
--- routes/medicalmodule.js
+++ routes/medicalmodule.js
@@ -9,9 +9,28 @@ var connection = require('../modules/connection');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-router.post('/', function(req, res) {
-    console.log('in the module', req.body);
+router.post('/:fakeId', function(req, res) {
+    //console.log('in the module', req.body);
+    var addMedical = {
+        intakeDate: req.body.intakeDate,
+        currentInjuries: req.body.currentInjuries,
+        first_name: req.params.fakeId
+    };
+    console.log('add medical var', addMedical);
+    pg.connect(connection, function (err, client, done) {
+        client.query("UPDATE client SET last_medical = $1, current_injuries = $2 WHERE first_name = $3",
+            [addMedical.intakeDate, addMedical.currentInjuries, addMedical.first_name],
+            function (err, result) {
+                done();
 
+                if (err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                } else {
+                    res.send(result);
+                }
+            });
+    });
 });
 
 
