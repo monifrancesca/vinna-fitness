bec1e2ead8a93d61699b465e6aa7985ece2a0a69
Moni Francesca <mo.francesca@gmail.com>
2016-03-29 10:29:11 -0500

    Initial work.

--- public/scripts/controllers/personalcontroller.js
+++ public/scripts/controllers/personalcontroller.js
@@ -2,4 +2,29 @@ myApp.controller('PersonalController', ['$scope', '$http', 'DataFactory', functi
 
   $scope.dataFactory = DataFactory;
 
+  //$scope.dataFactory.retrievePersonal().then(function() {
+  //  $scope.clientPersonal = $scope.dataFactory.clientInfo();
+    //console.log($scope.clientPersonal);
+
+  //  $scope.info = $scope.clientPersonal[0];
+  //  //console.log($scope.info);
+  //});
+
+  $scope.savePersonalInfo = function() {
+    var info = {
+      firstName: $scope.firstName,
+      lastName: $scope.lastName,
+      phoneNumber: $scope.phoneNumber,
+      emailAddress: $scope.emailAddress,
+      dateOfBirth: $scope.dateOfBirth,
+      heightFeet: $scope.heightFeet,
+      heightInches: $scope.heightInches,
+      weightPounds: $scope.weightPounds,
+      emergencyContactName: $scope.emergencyContactName,
+      emergencyContactNumber: $scope.emergencyContactNumber
+    };
+    console.log('info in controller', info);
+    $scope.dataFactory.sendPersonal(info);
+  };
+
 }]);
--- public/scripts/factories/data-factory.js
+++ public/scripts/factories/data-factory.js
@@ -1,14 +1,39 @@
 myApp.factory('DataFactory', ['$http', function($http) {
 
+  //PRIVATE
+
+  //var fakeIdentifier = 'a';
+  var clientPersonal = undefined;
+
+  var postPersonal = function(data) {
+    console.log('factory data', data);
+    $http.post('/personal/', data).then(function(response) {
+    });
+  };
+
+  //var getPersonal = function() {
+  //  console.log('getPersonal in factory fired');
+  //  var promise = $http.get('/personal/' + fakeIdentifier).then(function(response) {
+  //    clientPersonal = response.data;
+  //    console.log('clientPersonal', clientPersonal);
+  //  });
+  //  return promise;
+  //};
 
 
-
-
-
-
-
+  //PUBLIC
 
   var dataFactoryOutput = {
+    sendPersonal: function(info) {
+      console.log('in the factory', info);
+      postPersonal(info);
+    },
+    //retrievePersonal: function() {
+    //  return getPersonal();
+    //},
+    clientInfo: function() {
+      return clientPersonal;
+    }
 
   };
 
