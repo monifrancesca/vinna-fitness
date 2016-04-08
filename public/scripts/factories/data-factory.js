247b15d6fb6e45bde06c273c40e041a5df6a7481
Andrew Glenn <drstrangembm@gmail.com>
2016-04-08 09:21:51 -0500

    Got FMS looking pretty nice

--- public/scripts/factories/data-factory.js
+++ public/scripts/factories/data-factory.js
@@ -166,11 +166,10 @@ myApp.factory('DataFactory', ['$http', function($http) {
 
   var facPostFmsData = function(data) {
     console.log('posting fms data');
-    var promise = $http.post('/fms', data).then(function(response) {
+    $http.post('/fms', data).then(function(response) {
       console.log('match data saved');
       console.log(response);
     });
-    return promise;
   };
 
   var facGetFmsData = function() {
