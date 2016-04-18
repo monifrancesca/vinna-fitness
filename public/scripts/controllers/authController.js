2ec7d30aec3c3c7fb25ef452d577bd2b3f9ce0c0
Moni Francesca <mo.francesca@gmail.com>
2016-04-18 15:54:01 -0500

    changed file names to lowercase. these uppercase files where still hanging around

--- public/scripts/controllers/authController.js
+++
@@ -1,5 +0,0 @@
-myApp.controller('AuthController', function (AuthFactory) {
-  var _this = this;
-  var authFactory = AuthFactory;
-  _this.loggedIn = authFactory.checkLoggedIn(); // NOTE: only updated on page load
-});
