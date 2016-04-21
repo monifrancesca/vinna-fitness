a46e5c2a723e550578b027020e4c4aa86cb12e1c
Andrew Glenn <drstrangembm@gmail.com>
2016-04-21 17:48:52 -0500

    updated userid in fms object

--- public/scripts/controllers/navController.js
+++ public/scripts/controllers/navController.js
@@ -13,9 +13,12 @@ myApp.controller('NavController', function (AuthFactory, $window) {
       _this.displayLogout = true;
       authFactory.setLoggedIn(true);
       _this.username = response.data.name;
+      authFactory.setTrainerId(response.data.id);
+      console.log('hopes and dreams: '+authFactory.trainerId());
     } else { // is not logged in on server
       _this.displayLogout = false;
       authFactory.setLoggedIn(false);
+      authFactory.setTrainerId('');
     }
   },
 
@@ -28,6 +31,7 @@ myApp.controller('NavController', function (AuthFactory, $window) {
     authFactory.logout()
       .then(function (response) { // success
         authFactory.setLoggedIn(false);
+          authFactory.setTrainerId('');
         _this.username = '';
         $window.location.href = '/'; // forces a page reload which will update our NavController
       },
